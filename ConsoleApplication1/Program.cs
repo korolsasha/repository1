bbc699a7ffb93c7052de0bfc89d9bbd4252f61da
Sasha <koros.korol2003@gmail.com>
2021-09-10 09:38:32 +0300

    second commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
         }
     }
 }
